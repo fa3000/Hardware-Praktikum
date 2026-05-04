bed416981edc0c20aa370bf843fb510f3f32c609
Fabian Gauss <fg66@email.uni-freiburg.de>
2026-05-04 22:27:54 +0200

    Added ex01

--- uebungsblatt-01/ex01/HWP_Sheet_01_Task1_Template/HWP_Sheet_01_Task1_Template.ino
+++ uebungsblatt-01/ex01/HWP_Sheet_01_Task1_Template/HWP_Sheet_01_Task1_Template.ino
@@ -13,13 +13,22 @@ bool          led_on        = false; // current LED state
 // TODO (Bonus): add a counter for completed blink cycles
 //               and a variable to track the current colour
 
+uint8_t cycles_counter = 0;
+uint8_t current_color = 0;
+
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
+  // while (!Serial);
   // Set LED pins as outputs.
   // pinMode(pin, OUTPUT) configures a pin for digital output
   // Start with all LEDs off (active-low → HIGH = off)
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
 }
 
 
@@ -35,6 +44,12 @@ void loop() {
   //       turn LED_RED off,
   //       wait BLINK_INTERVAL_MS ms.
 
+  /*
+  digitalWrite(LED_RED, LOW);
+  delay(BLINK_INTERVAL_MS);
+  digitalWrite(LED_RED, HIGH);
+  delay(BLINK_INTERVAL_MS);
+  */
 
   // ----------------------------------------------------------
   //  Task 1 ii) — Blink using millis()  (comment out i) first)
@@ -43,11 +58,30 @@ void loop() {
   // TODO: check whether BLINK_INTERVAL_MS has elapsed since last_blink_ms.
   // TODO: if yes — update last_blink_ms, toggle led_on,
   //               and write the correct HIGH/LOW to LED_RED.
+  
+  /*
+  unsigned long current_time = millis();
 
+  if(current_time - last_blink_ms > BLINK_INTERVAL_MS){
+    last_blink_ms = current_time;
+    if(led_on==false){
+      digitalWrite(LED_RED, LOW);
+      led_on = true;
+    }
+    else{      
+      digitalWrite(LED_RED, HIGH);
+      led_on = false;
+    }
+  }
+  */
 
   // ----------------------------------------------------------
   //  Task 1 iii) — Answer as a comment
   // ----------------------------------------------------------
+
+  /*Mit delay() wird einfach für den festgelegten Zeitraum der Seeed blockiert und nichts wird ausgeführt. 
+  Mit dem millis() Ansatz aus ii) wird der Seeed nicht blockiert und während dem warten können auch andere Aktionen ausgeführt werden.*/
+
 
   // ----------------------------------------------------------
   //  Bonus — Colour cycling after 10 blink cycles
@@ -57,4 +91,51 @@ void loop() {
   //         red → green → blue → red → ...
   //       Turn off all LEDs before switching, then turn on only
   //       the new active colour.
+
+  unsigned long current_time = millis();
+
+  if(cycles_counter > 9){
+    cycles_counter = 0;
+    current_color = (current_color + 1) % 3;
+  }
+
+  if(current_time - last_blink_ms > BLINK_INTERVAL_MS){
+    last_blink_ms = current_time;
+    if(current_color == 0){
+      if(led_on==false){
+        digitalWrite(LED_RED, LOW);
+        led_on = true;
+      }
+      else{      
+        digitalWrite(LED_RED, HIGH);
+        led_on = false;
+        cycles_counter += 1;
+      }
+    }
+        if(current_color == 1){
+      if(led_on==false){
+        digitalWrite(LED_GREEN, LOW);
+        led_on = true;
+      }
+      else{      
+        digitalWrite(LED_GREEN, HIGH);
+        led_on = false;
+        cycles_counter += 1;
+      }
+    }
+        if(current_color == 2){
+      if(led_on==false){
+        digitalWrite(LED_BLUE, LOW);
+        led_on = true;
+      }
+      else{      
+        digitalWrite(LED_BLUE, HIGH);
+        led_on = false;
+        cycles_counter += 1;
+      }
+    }
+  }
+
+
+
 }
