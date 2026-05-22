e30eb4c27ca52fa0917fced79ca9bdfbae24d606
Matthias Sonner <ms2585@email.uni-freiburg.de>
2026-05-22 14:43:56 +0200

    Task4, Task 2-3 adjustet

--- uebungsblatt-02/ex02/exercise_templates/task2and3/task2and3.ino
+++ uebungsblatt-02/ex02/exercise_templates/task2and3/task2and3.ino
@@ -11,11 +11,19 @@
 
 
 void setup() {
-
-  NRF_P0->DIRSET = (1UL << 29);     // Set Buzzer for Task 2
+  NRF_TIMER1->MODE = TIMER_MODE_MODE_Timer;           // define mode
+  NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_32Bit;  // define bitmode
+  NRF_TIMER1->PRESCALER = 4;                          // define prescaler for 1000hz
+  NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;   
+  NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+  NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Msk;
   
-  NRF_P0->DIRCLR = (1UL << 3);    // set button for Task 3
-  NRF_P0->PIN_CNF[3] =            //  activate button
+  NVIC_EnableIRQ(TIMER1_IRQn);                      // enable interrupt
+
+  NRF_P0->DIRSET = (1UL << 29);                     // Set Buzzer for Task 2/3
+  NRF_P0->DIRCLR = (1UL << 3);                      // set button for Task 3
+  
+  NRF_P0->PIN_CNF[3] =                              //  activate button
     (GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos) |
     (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos) |
     (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos);
@@ -31,34 +39,27 @@ void loop() {
   bool pressed = !(NRF_P0->IN & (1UL << 3)); // check if button is pressed(active low)
   
   if(pressed && !wasPressed){                 // check for switch
-    isPlay = !isPlay;                     // toggle state
+    isPlay = !isPlay;                         // toggle state
   }
-
-  wasPressed = pressed;              // remember state
+  
+  delay(20);                                  //avoid doubleclicks
+  
+  wasPressed = pressed;                       // remember state
 
   if(isPlay){
-    NRF_TIMER1->TASKS_START = 1;    // Start timer
+    NRF_TIMER1->TASKS_START = 1;              // Start timer
   }
   else {
-    NRF_TIMER1->TASKS_STOP = 1;     // stop timer 
+    NRF_TIMER1->TASKS_STOP = 1;               // stop timer 
     NRF_P0->OUTCLR = (1UL << 29);
   }
 }
 
 
 void setTimer1Freq() {
-  NRF_TIMER1->MODE = TIMER_MODE_MODE_Timer;           // define mode
-  NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_32Bit;  // define bitmode
-  NRF_TIMER1->PRESCALER = 4;                          // define prescaler for 1000hz
-
-  uint32_t ticks = 1000000/ (2*1046);                     //calculate ticks 
+  uint32_t ticks = 1000000/ (2*1046);        //calculate ticks 
   
-  NRF_TIMER1->CC[0] = ticks ;                             //capture/compare 
-  NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;   
-  NRF_TIMER1->EVENTS_COMPARE[0] = 0;
-  NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Msk;
-
-  NVIC_EnableIRQ(TIMER1_IRQn);    // enable interrupt
+  NRF_TIMER1->CC[0] = ticks ;                //capture/compare 
 
   NRF_TIMER1->TASKS_CLEAR = 1;    
   NRF_TIMER1->TASKS_START = 1;
@@ -73,14 +74,14 @@ void setBuzzerFreq() {
 
 
 extern "C" void TIMER1_IRQHandler() {
-  if(NRF_TIMER1->EVENTS_COMPARE[0]){
-    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+  if(NRF_TIMER1->EVENTS_COMPARE[0]){     //check for event
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;   //clear event
   
-    if(NRF_P0->OUT & (1UL << 29)){  //check if Pin is high
-    NRF_P0->OUTCLR = (1UL << 29); // Set Buzzer low
+    if(NRF_P0->OUT & (1UL << 29)){       //check if Pin is high
+    NRF_P0->OUTCLR = (1UL << 29);        // Set Buzzer low
     }
     else{
-    NRF_P0->OUTSET = (1UL << 29); // Set Buzzer high
+    NRF_P0->OUTSET = (1UL << 29);        // Set Buzzer high
     }
   }
 }
--- uebungsblatt-02/ex02/exercise_templates/task4/task4.ino
+++ uebungsblatt-02/ex02/exercise_templates/task4/task4.ino
@@ -9,13 +9,23 @@
 
 
 void setup() {
+  NRF_TIMER1->MODE = TIMER_MODE_MODE_Timer;           // define mode
+  NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_32Bit;  // define bitmode
+  NRF_TIMER1->PRESCALER = 4;                          // define prescaler for 1000hz
+  NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;   
+  NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+  NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Msk;
+  
+  NVIC_EnableIRQ(TIMER1_IRQn);                        // enable interrupt
 
-
+  NRF_P0->DIRSET = (1UL << 29);                       // Set Buzzer for Task 2/3 
 }
 
 
 void loop() {
 // tests
+  setBuzzerFreq(50);  //A4
+  delay(300);
   setBuzzerFreq(440);  //A4
   delay(300);
   setBuzzerFreq(554);  //C#5
@@ -33,10 +43,32 @@ void loop() {
 
 
 void setBuzzerFreq(uint32_t freq) {
+  if(freq > 3000 || freq < 100){              //check out of range
+      NRF_P0->OUTCLR = (1UL << 29);           // Set Buzzer low
+      NRF_TIMER1->TASKS_STOP = 1;
+      NRF_TIMER1->TASKS_CLEAR = 1;
+  }
+  else {
+  uint32_t ticks = 1000000/ (2*freq);         //calculate ticks uses param instead of 1046
+    NRF_TIMER1->TASKS_STOP = 1;               //stop timer
+    NRF_TIMER1->TASKS_CLEAR = 1;              //clear timer
 
+    NRF_TIMER1->CC[0] = ticks ;               //capture/compare 
+
+    NRF_TIMER1->TASKS_START = 1;              // start timer
+  }
 }
 
 
 extern "C" void TIMER1_IRQHandler() {
-
+  if(NRF_TIMER1->EVENTS_COMPARE[0]){          //check for event
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;        //clear event
+  
+    if(NRF_P0->OUT & (1UL << 29)){            //check if Pin is high
+    NRF_P0->OUTCLR = (1UL << 29);             // Set Buzzer low
+    }
+    else{
+    NRF_P0->OUTSET = (1UL << 29);             // Set Buzzer high
+    }
+  }
 }
